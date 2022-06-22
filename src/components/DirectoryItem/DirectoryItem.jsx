c85f12cdc733e5332c69509a7152dbaf2229d53b
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-06-22 11:09:31 +0300

    navigate in directory

--- src/components/DirectoryItem/DirectoryItem.jsx
+++ src/components/DirectoryItem/DirectoryItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { DirectoryBody, DirectoryItemContainer, BackgroundImage } from './DirectoryItem.styles.jsx';
+import { DirectoryBody, DirectoryItemContainer, BackgroundImage } from './DirectoryItem.styles';
 
 function DirectoryItem({ title, imageUrl }) {
+	const navigate = useNavigate();
+
+	const handleNavigate = () => {
+		navigate(`shop/${title}`);
+	};
+
 	return (
-		<DirectoryItemContainer>
+		<DirectoryItemContainer onClick={handleNavigate}>
 			<BackgroundImage imageUrl={imageUrl}></BackgroundImage>
 			<DirectoryBody>
 				<h2>{title}</h2>
